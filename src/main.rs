020426509121544e70d51528c242ef888836e4db
Todd Stellanova <tstellanova@gmail.com>
2018-02-20 17:28:16 +0000

    Use constant for temperature file path

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,23 @@ use std::io::Read;
 use std::{thread, time};
 
 
+// Raspberry Pi makes CPU temperature available in this file path:
+const CPU_TEMPERATURE_PATH : &'static str =  "/sys/class/thermal/thermal_zone0/temp";
 
 /// Read the RPi's CPU temperature and write it to stdout
 ///
 fn read_and_display_temperature() {
-  let fname = "/sys/class/thermal/thermal_zone0/temp";
-  let mut f = File::open(fname)
-    .expect("file not found");
+  let mut f = File::open(CPU_TEMPERATURE_PATH)
+    .expect("Could not open CPU_TEMPERATURE_PATH");
 
-  let mut temp_str= String::new();
+  let mut temp_str = String::new();
   f.read_to_string(&mut temp_str)
-    .expect("something went wrong reading the file");
+    .expect("Could not read temperature");
 
   let raw_temp : u32 = temp_str.trim().parse()
     .expect("Bogus raw temperature string");
 
   let temp_float : f32 = {raw_temp as f32} / 1000.0;
-
   println!("{}",temp_float);
 }
 
